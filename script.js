f8f2777fca3fbb0740fcb6f68fb44dabe55d8a70
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-26 14:18:35 -0300

    Update script.js
    Adicionando animação das letras através da variavel typingEffect  e a animação de carregação do Home

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* -------- NAVIGATION BAR FUNCTION -------- */
 function menuFunction(){
-    var menuBtn = document.getElementById("myNavMenu");
+    let menuBtn = document.getElementById("myNavMenu");
 
     if (menuBtn.className === "nav-menu") {
         menuBtn.className += " responsive";
@@ -28,11 +28,30 @@ function headerShadow(){
 }
 
 /* -------- TYPING EFFECT -------- */
+    let typingEffect = new Typed(".typedText", {
+        strings : ["Desenvolvedor","Backend Java", "FullStack"],
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 2000
+    });
 
 /* -------- SCROLL REVEAL ANIMATION -------- */
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+    });
 
 /* -------- HOME -------- */
-
+    sr.reveal('.featured-text-card',{})
+    sr.reveal('.featured-name',{delay: 100})
+    sr.reveal('.featured-text-info',{delay: 200})
+    sr.reveal('.featured-text-btn',{delay: 200})
+    sr.reveal('.social_icons',{delay: 200})
+    sr.reveal('.featured-image',{delay: 300})
+    
 /* -------- PROJECT BOX -------- */
 
 /* -------- HEADINGS -------- */
