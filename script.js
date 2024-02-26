402e4da0ec9b14c5afdddcb01937eac36b4b28c2
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-26 01:03:46 -0300

    Update script.js
    Criação da função para adicionar sombra ao header

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ function menuFunction(){
 }
 
 /* -------- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING -------- */
+window.onscroll = function(){headerShadow()};
+
+function headerShadow(){
+    const navHeader = document.getElementById("header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0,0,0,0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    } else {
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+
+}
 
 /* -------- TYPING EFFECT -------- */
 
