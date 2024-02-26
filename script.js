bbc3331a730fe669f2d343a85abb10c35c53aed6
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-26 14:50:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -81,4 +81,21 @@ const srRight = ScrollReveal({
       
  srRight.reveal('.skills-box',{delay: 100});
  srRight.reveal('.form-control',{delay: 100});
-      
+
+/* ----- CHANGE ACTIVE LINK ----- */
+  
+const sections = document.querySelectorAll('section[id]')
+function scrollActive() {
+  const scrollY = window.scrollY;
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute('id');
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
+    }  else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
