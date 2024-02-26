f9624566943e6dbe51e179878338044d3c5bb945
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-25 22:47:04 -0300

    Update script.js
    -Criação da função para o controle do menu bar

---
+++ script.js
@@ -0,0 +1,24 @@
+/* -------- NAVIGATION BAR FUNCTION -------- */
+function menuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if (menuBtn.className === "nav-menu") {
+        menuBtn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+/* -------- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING -------- */
+
+/* -------- TYPING EFFECT -------- */
+
+/* -------- SCROLL REVEAL ANIMATION -------- */
+
+/* -------- HOME -------- */
+
+/* -------- PROJECT BOX -------- */
+
+/* -------- HEADINGS -------- */
+
+/* -------- SCROLL REVEAL LEFT_RIGHT ANIMATION -------- */
